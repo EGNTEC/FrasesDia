cdfc2bd686b16fa959569ec13823fee2856c2448
egntec <emerson.gonas@gmail.com>
2018-07-06 21:35:55 -0300

    Finalizando o projeto

--- App.js
+++ App.js
@@ -22,7 +22,20 @@ const Estilos = StyleSheet.create({
 
 });
 
-const botaoPressionado = () =>{  
+const gerarNovaFrase = () =>{ 
+  var numAleatorio = Math.random();
+  numAleatorio = Math.floor(numAleatorio*5);
+
+  var frases = Array();
+  frases[0]='Nunca se ache demais. Pois tudo que é demais, sobra. Tudo que é sobra, é resto. E tudo que é resto vai para o lixo.';
+  frases[1]='A nossa maior glória não reside no fato de nunca cairmos, mas sim em levantarmo-nos sempre depois de cada queda.';
+  frases[2]='Nunca troque o que mais quer na vida por aquilo que mais quer no momento. Momentos passam, a vida continua.';
+  frases[3]='O sábio nunca diz tudo o que pensa, mas pensa sempre tudo o que diz.';
+  frases[4]='É sempre a hora certa, para fazer a coisa certa.';
+  
+  var fraseEscolha = frases[numAleatorio];
+
+  alert(fraseEscolha);
 }
 
 //Criar o componente
@@ -34,7 +47,9 @@ export default class App extends Component {
        <View style={principal}>
 
           <Image source={require('./img/logo.png')} />
-          <TouchableOpacity style={botao}>
+          <TouchableOpacity 
+              onPress={gerarNovaFrase}
+              style={botao}>
               <Text style={textoBotao}>Nova Frase</Text>
           </TouchableOpacity>  
           
